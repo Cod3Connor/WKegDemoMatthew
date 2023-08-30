6acfde26298f6700c96ffb4f636a3c71e6478bd6
Matthew <hazzardboe@gmail.com>
2023-08-30 10:23:53 -0600

    d

---
+++ main.js
@@ -0,0 +1,9 @@
+function goToSpot(){
+    console.log("Going to spot");
+}
+function goToStats(){
+    console.log("Going to stats");
+}  
+function goToReport(){
+    console.log("Going to report");
+}
